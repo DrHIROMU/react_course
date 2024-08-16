060ab39d2d4dd3dc361566f919bddb49f8b1dd32
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2024-08-16 10:32:28 +0800

    investment calculator finished

--- 05-React_Essentials_Practice_Project/01-starting-project/src/App.jsx
+++ 05-React_Essentials_Practice_Project/01-starting-project/src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
+import Results from "./components/Results";
 import UserInput from "./components/UserInput";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      console.log(inputIdentifier, newValue);
+      return { ...prevUserInput, [inputIdentifier]: +newValue };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {inputIsValid ? (
+        <Results input={userInput} />
+      ) : (
+        <p className="center">Please enter a valid input</p>
+      )}
     </>
   );
 }

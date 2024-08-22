8d79a7f7ee8c5ea89e3ef94b74175bbbc7afbbaf
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2024-08-22 10:08:29 +0800

    add tailwind

--- 06-Styling_React_Components/01-starting-project/src/components/AuthInputs.jsx
+++ 06-Styling_React_Components/01-starting-project/src/components/AuthInputs.jsx
@@ -77,9 +77,7 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
-          Sign In
-        </button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
---
+++ 06-Styling_React_Components/01-starting-project/src/components/Button.jsx
@@ -0,0 +1,3 @@
+export default Button({children, ...props}){
+  return <button className="px-4 py-2 font-semifold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-500" {...props}>{children}</button>
+}

0a64be792a16bf61024feafe24ce1fed726e50be
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2024-08-23 10:15:47 +0800

    tailwind style complete

---
+++ 06-Styling_React_Components/01-starting-project/src/components/Input.jsx
@@ -0,0 +1,25 @@
+export default function Input({ label, invalid, ...props }) {
+  let labelClass = "block mb-2 text-xs font-bold tracking-wide uppercase";
+  let inputClass = "w-full px-3 py-2 leading-tight border rounded shadow";
+
+  if (invalid) {
+    labelClass += " text-red-400";
+  } else {
+    labelClass += " text-stone-300";
+  }
+
+  if (invalid) {
+    inputClass += " text-red-400";
+    inputClass += " text-red-500 bg-red-100 border-red-300";
+  } else {
+    inputClass += " text-stone-300";
+    inputClass += " text-gray-700 bg-stone-300";
+  }
+
+  return (
+    <p>
+      <label className={labelClass}>{label}</label>
+      <input className={inputClass} {...props} />
+    </p>
+  );
+}

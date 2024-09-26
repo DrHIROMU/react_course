4174e14a07877ce217f7e8c1fc865296332cff25
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2024-09-26 13:05:02 +0800

    quiz complete

--- 12-Practice_Project_Building_a_Quiz_App/01-starting-project/src/components/Quiz.jsx
+++ 12-Practice_Project_Building_a_Quiz_App/01-starting-project/src/components/Quiz.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 
 import QUESTIONS from "../question.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectedAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
@@ -13,11 +15,23 @@ export default function Quiz() {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="question">
       <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
       <ul id="answers">
-        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+        {shuffledAnswers.map((answer) => (
           <li key={answer} className="answer">
             <button onClick={() => handleSelectedAnswer(answer)}>
               {answer}

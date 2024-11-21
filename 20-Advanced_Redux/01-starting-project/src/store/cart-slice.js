e161dbdb18b67ca642da1d6337c1ea96a962764c
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2024-11-21 09:55:56 +0800

    redux slice complete

--- 20-Advanced_Redux/01-starting-project/src/store/cart-slice.js
+++ 20-Advanced_Redux/01-starting-project/src/store/cart-slice.js
@@ -7,6 +7,8 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+
       if (!existingItem) {
         state.items.push({
           itemId: newItem.id,
@@ -20,7 +22,18 @@ const cartSlice = createSlice({
         existingItem.totalPrice += newItem.price;
       }
     },
-    removeItemFromCart(state, action) {},
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice -= existingItem.price;
+      }
+    },
   },
 });
 
--- 20-Advanced_Redux/01-starting-project/src/store/index.js
+++ 20-Advanced_Redux/01-starting-project/src/store/index.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import uiReducer from "./ui-slice";
+import cartReducer from "./cart-slice";
+
+//https://redux-test-911cd-default-rtdb.asia-southeast1.firebasedatabase.app/
 
 const store = configureStore({
-  reducer: { ui: uiReducer },
+  reducer: { ui: uiReducer, cart: cartReducer },
 });
 
 export default store;

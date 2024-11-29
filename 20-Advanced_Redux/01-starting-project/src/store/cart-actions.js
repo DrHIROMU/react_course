684498e4b6ef6894d567ce101eccce217b519aed
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2024-11-29 09:48:54 +0800

    shopping cart app with redux complete

--- 20-Advanced_Redux/01-starting-project/src/store/cart-actions.js
+++ 20-Advanced_Redux/01-starting-project/src/store/cart-actions.js
@@ -19,8 +19,14 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-
-      dispatch(cartActions.replaceCart(cartData));
+      if (cartData != null) {
+        dispatch(
+          cartActions.replaceCart({
+            items: cartData.items || [],
+            totalQuantity: cartData.totalQuantity,
+          })
+        );
+      }
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -46,7 +52,13 @@ export const sendCartData = (cart) => {
     const sendRequest = async () => {
       const response = await fetch(
         "https://redux-test-911cd-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
       );
 
       if (!response.ok) {
--- 20-Advanced_Redux/01-starting-project/src/store/cart-slice.js
+++ 20-Advanced_Redux/01-starting-project/src/store/cart-slice.js
@@ -2,7 +2,12 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
   name: "cart",
-  initialState: { items: [], totalQuantity: 0, totalAmount: 0 },
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+    totalAmount: 0,
+    changed: false,
+  },
   reducers: {
     replaceCart(state, action) {
       state.totalQuantity = action.payload.totalQuantity;
@@ -12,10 +17,11 @@ const cartSlice = createSlice({
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
 
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -30,6 +36,7 @@ const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
+      state.changed = true;
 
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);

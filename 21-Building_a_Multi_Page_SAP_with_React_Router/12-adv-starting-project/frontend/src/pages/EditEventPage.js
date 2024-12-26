6c6d3b5a37a09d2e1cb627812234a21907b2d3f2
Lin Kung Hung <drhiromu@gmail.com>
2024-12-26 23:58:56 +0800

    useRouteLoaderData() to get the loader function data

--- 21-Building_a_Multi_Page_SAP_with_React_Router/12-adv-starting-project/frontend/src/pages/EditEventPage.js
+++ 21-Building_a_Multi_Page_SAP_with_React_Router/12-adv-starting-project/frontend/src/pages/EditEventPage.js
@@ -1,9 +1,10 @@
+import { useRouteLoaderData } from "react-router-dom";
+import EventForm from "../components/EventForm";
+
 const EditEventPage = () => {
-  return (
-    <div>
-      <h1>EditEventPage</h1>
-    </div>
-  );
+  const data = useRouteLoaderData("event-detail ");
+
+  return <EventForm event={data.event} />;
 };
 
 export default EditEventPage;

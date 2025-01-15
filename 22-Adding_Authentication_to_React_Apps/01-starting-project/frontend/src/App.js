202b8bfd19fdc3ff1896e2a08538b94977b5a23c
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2025-01-15 10:09:21 +0800

    1. logout 2. Different pages before/after login

--- 22-Adding_Authentication_to_React_Apps/01-starting-project/frontend/src/App.js
+++ 22-Adding_Authentication_to_React_Apps/01-starting-project/frontend/src/App.js
@@ -12,16 +12,20 @@ import HomePage from "./pages/Home";
 import NewEventPage from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 import { action as manipulateEventAction } from "./components/EventForm";
+import { action as logoutAction } from "./pages/Logout";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 import AuthenticationPage, {
   action as authAction,
 } from "./pages/Authentication";
+import { tokenLoader } from "./util/auth";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id: "root",
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
       {
@@ -66,6 +70,10 @@ const router = createBrowserRouter([
         path: "newsletter",
         element: <NewsletterPage />,
         action: newsletterAction,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
       },
     ],
   },
